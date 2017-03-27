3ca69dcf25aa1d12db1885fe684cd5d152b99a4e
thapapratyush <thapa.pratyush10@gmail.com>
2017-03-26 22:44:25 -0400

    Created the tray. Panning enabled
    d

---
+++ Lab-week7/CanvasViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CanvasViewController.swift
+//  Lab-week7
+//
+//  Created by Pratyush Thapa on 3/26/17.
+//  Copyright © 2017 CodePath-DC. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
